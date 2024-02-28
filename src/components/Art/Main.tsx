5bfd418f4f1acdcb5942aef8d0a8834c3ca330f6
marcellinoputra <marcellinoputra@github.com>
2024-02-29 01:21:33 +0700

    adding art menu and sub menu art

---
+++ src/components/Art/Main.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { Box, Tab } from '@mui/material';
+import { useState } from 'react';
+
+export default function Art() {
+  const [selectedTab, setSelectedTab] = useState('1');
+
+  function handleChangeTabs(event: React.SyntheticEvent, newTabs: string) {
+    setSelectedTab(newTabs);
+  }
+
+  return (
+    <>
+      <Box>
+        <TabContext value={selectedTab}>
+          <Box
+            sx={{
+              borderBottom: 1,
+              borderColor: 'divider',
+            }}
+          >
+            <TabList
+              aria-label="Tabs Art"
+              onChange={handleChangeTabs}
+              textColor="primary"
+              indicatorColor="secondary"
+            >
+              <Tab label="Classic Art" value="1" />
+              <Tab label="Modern Art" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Tabs For Classic Art</TabPanel>
+          <TabPanel value="2">Tabs For Modern Art</TabPanel>
+        </TabContext>
+      </Box>
+    </>
+  );
+}
